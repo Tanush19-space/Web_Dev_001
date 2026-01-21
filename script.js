ae7fdbf4ee4b6e11c81ad67414b7a9ada4b2770e
Tanush19-space <sat.tyagi0707@gmail.com>
2026-01-21 15:04:55 +0530

    first code

---
+++ script.js
@@ -0,0 +1,67 @@
+// const para=document.querySelectorAll('.para')
+// para.innerText="This is updated paragraph"
+// para.style.color="red"
+// console.log(para)
+
+// const para=document.getElementsByClassName('para')
+// console.log(para)
+
+// const para2=document.querySelector('.para')
+// console.log(para2)
+
+// const para3=document.querySelectorAll('.para')
+// console.log(para3)
+
+// let arr=[23,45,54,55,2,333,4]
+// arr.forEach((ele)=>console.log(ele))
+
+// const para=document.querySelector('para')
+// para.textcontent="This is a updated paragraph"
+// para.style.color="red"
+// console.log(para)
+
+// const container=document.querySelector(".container")
+// container.innerHTML="<h1>This is a heading"
+// console.log(container)
+
+// const btn=document.querySelector('.submit')
+// btn.classList.add("btn")
+// btn.classList.remove("btn")
+// btn.classList.toggle('btn')
+
+// function print(){
+//     console.log("Hello you have clicked the button")
+// }
+
+// btn.addEventListener('click', print)
+// btn.addEventListener('click', ()=>console.log("Hello you have clicked the btn"))
+// btn.removeEventListener('click', print)
+
+// const click=document.querySelector('#click')
+// const stop=document.querySelector('#stop')
+
+// function Message(event){
+//     console.log(event.key)
+//     console.log("Hello you have clicked the button")
+// }
+
+// click.addEventListener('keyup', Message)
+// stop.addEventListener('click', ()=>{
+//     console.log("You have stopped the button")
+//     click.removeEventListener('keyup', Message)
+// })
+
+// const form=document.querySelector('form')
+// form.addEventListener('submit', (event)=>{
+//     event.preventDefault()
+//     console.log("Form Submitted")
+// })
+
+// const div=document.querySelector('.container')
+// const div2=document.querySelector('outer')
+// const btn=document.querySelector('button')
+
+// div.addEventListener('click', ()=>{console.log("Clicked the div")}, true)
+// div2.addEventListener('click', ()=>{console.log("Clicked the outer div")}, true)
+// btn.addEventListener('click', ()=>{console.log("Clicked the button")}, true)
+
